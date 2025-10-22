2cb41a4ea13495d4d3f6447b5ef3cca41d1d55e4
christobass <67762028+christobass@users.noreply.github.com>
2025-10-22 12:09:10 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // ⚠️ IMPORTANT : Remplacez par le nom EXACT de votre repo GitHub
-  base: '/christobass/commandes',
+  base: '/commandes',
 })
